262fa9d93a1e63b1cf3654b0e8ae5c08a2953a6d
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-24 10:41:24 -0500

    got the response coming back from axios post. working.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,27 @@
 import React from "react";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
-const Login = () => {
+const Login = () =>
+{
   // make a post request to retrieve a token from the api
+
+  const handleClick = (e) =>{
+    e.preventDefault()
+    const creds = { username: 'Lambda School', password: 'i<3Lambd4' }
+    axiosWithAuth()
+    .post("/api/login", creds)
+    .then(res=> {
+      console.log("response", res)
+    })
+  }
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <br/>
-      <p>Build a login page here</p>
+      <br />
+      <form action="">
+        <button onClick={handleClick}>Login</button>
+      </form>
     </>
   );
 };
