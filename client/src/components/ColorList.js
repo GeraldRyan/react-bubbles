b397e2500280e4b0e8e2be51537ebce21f70f498
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-24 11:52:02 -0500

    the put and delete seem to run but have no effect

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,42 +1,52 @@
 import React, { useState } from "react";
 import axios from "axios";
-import {axiosWithAuth} from '../utils/axiosWithAuth'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+import { useHistory } from 'react-router-dom'
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors }) =>
+{
   console.log("Colors", colors);
-  
+
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
-  const editColor = color => {
+  const editColor = color =>
+  {
     setEditing(true);
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = e =>
+  {
     e.preventDefault();
     // Make a put request to save your updated color
     axiosWithAuth()
-    .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
-    .then(res =>{
-      console.log("Response from color editing. Id=", colorToEdit.id)
-      console.log("Color to edit", colorToEdit)
-    })
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res =>
+      {
+        console.log("Response from color editing. Id=", colorToEdit.id)
+        console.log("Color to edit", colorToEdit)
+
+      })
     // think about where will you get the id from...
     // where is is saved right now?
   };
 
-  const deleteColor = color => {
+  const deleteColor = color =>
+  {
     // make a delete request to delete this color
     axiosWithAuth()
-    .delete("/api/colors/${colors.id}")
-    .then(res=>{}
-      )
+      .delete(`/api/colors/${colorToEdit.id}`)
+      .then(res =>
+      {
+        console.log("response from delete request", res)
+        
+      })
   };
 
   return (
@@ -46,12 +56,13 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span className="delete" onClick={e =>
+              {
+                e.stopPropagation();
+                deleteColor(color)
+              }
+              }>
+                x
               </span>{" "}
               {color.color}
             </span>
